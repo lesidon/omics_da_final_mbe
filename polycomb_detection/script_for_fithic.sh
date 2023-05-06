57709ec10dc15ce100bf36cfaef02a59ebb7eb07
lesidon <leon.sidorov123@gmail.com>
2023-05-06 19:23:46 +0300

    Finished the script for Fit-Hi-C

--- polycomb_detection/script_for_fithic.sh
+++ polycomb_detection/script_for_fithic.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+# this command converts the .hic into .mcool 
+hic2cool convert /gss/home/l.sidorov/omics_final_project/data/GSE168524_neurons_fem_wt_allValidPairs.hic \
+/gss/home/l.sidorov/omics_final_project/GSE168524_neurons_fem_wt_allValidPairs.mcool -p 1
 
+# this python script creates input files for Fit-Hi-C
+python3 interactions_and_fragments_file.py
+
+# this command generated the bias file
+python3 ../../fithic/fithic/utils/HiCKRy.py -i ./fithic_inputs/interactions.txt.gzip -f ./fithic_inputs/fragments.gzip -o ./fithic_inputs -x 0.05
+
+# this command creates a file with contact enrichment
+fithic -i ./fithic_inputs/interactions.txt.gzip -f ./fithic_inputs/fragments2.gzip -o ../output_fithic/ -r 100000 -t ./fithic_inputs/bias_file
